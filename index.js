5a4f4220fa1350cd2e229b92c4dde68671c51515
Moises Ramirez <moises.ramirez90@gmail.com>
2015-06-30 22:06:03 -0700

    complete chapter 5 exercises

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 'use strict';
-var mod1 = require('flattening');
+var arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+var flatten = require('flattening');
+console.log('Flattening:\n',flatten (arrays));
+// → [1, 2, 3, 4, 5, 6]
 
-var testArr = [
-  [8,0,5,8,2,2,3,6,8,1],
-  ['the','force','is','strong','in','this','one'],
-  ['one','ring','to','rule','them','all']
-];
-
-// console.log(mod1.flattenArrs(testArr));
 
 var ancestors = JSON.parse( require('ancestry') );
-var mod2 = require('age-diff');
-console.log(mod2.meanMomWhenChildAge(ancestors));
+// Your code here.
+var motherChildAgeDiff = require('age-diff');
+console.log('\n\nMother-Child Age Difference:\n', motherChildAgeDiff(ancestors));
+// → 31.2
+
+
+// Your code here.
+var averageAgePerCentury = require('age-per-centry');
+console.log('\n\nHistorical Life Expectancy:\n', averageAgePerCentury(ancestors));
+// → 16: 43.5
+//   17: 51.2
+//   18: 52.8
+//   19: 54.8
+//   20: 84.7
+//   21: 94 
+
+// Your code here.
+var every = require('every'),
+    some = require('some');
+console.log('\n\nEvery And Then Some:\n',every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
